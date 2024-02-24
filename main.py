6885bea1f8a94ea588c4bc5c63a9b49a94bc7839
Aaron Kelvin De Vroede <116379923+AaronDV17@users.noreply.github.com>
2024-02-24 17:04:56 +0400

    Altered select function docstrings. Removed redundant variable 'hitstay' in game()

--- main.py
+++ main.py
@@ -25,13 +25,17 @@ def clear():
 
 def deal(usr, no_cards):
   """
-  Deals the card holder passed as an argument a stated number of cards.
+  Deals the card holder a stated number of cards.
   """
   for i in range(0, no_cards):
     delt = choice(cards)
     usr.append(delt)
 
 def ace_check(usr, score):
+  """
+  Changes an ace's value in the card holder's hand from 11 to 1
+  if their total score is above 21
+  """
   if score > 21 and 11 in usr:
     ace_pos = usr.index(11)
     usr[ace_pos] = 1
@@ -66,16 +70,16 @@ def game():
     print("You win!")
   else:
     ace_check(dealer, d_score)
-    ace_check(player, p_score)
     d_score = sum(dealer)
+
+    ace_check(player, p_score)
     p_score = sum(player)
 
     print(f"The dealer's upcard is [{dealer[0]}]")
     print(f"Your cards are {player} with a value of {p_score}")
 
     while p_score <= 21:
-      hitstay = input("Would you like to hit (type 'y') or stay (type 'n')?\n")
-      if hitstay == 'y':
+      if input("Would you like to hit (type 'y') or stay (type 'n')?\n") == 'y':
         deal(player, 1)
         ace_check(player, p_score)
         p_score = sum(player)
